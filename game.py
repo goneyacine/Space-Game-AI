33c05a6fd49fde8cc5827995a73729e41d68a156
Yacine Betouche <goneyacine@gmail.com>
2023-11-21 14:37:55 +0100

    set up model train function and fix bugs

--- game.py
+++ game.py
@@ -2,60 +2,99 @@
 import pygame
 import math
 import random
-
+import numpy as np
+import tensorflow as tf
+import model
+import tensorflow_probability as tfp
 def start_game():
- MAX_OBSTICALS = 5
+ pg_model = model.pg_model()
+ MAX_OBSTICALS = 20
  PLAYER_POS_RANGE = (20,480) 
  OBSITCALS_X_POS_RANGE = 500
  OBSTICAL_MAX_Y_POS = 600
- OBSTICAL_MAX_SPEED = 0.2
+ OBSTICAL_MAX_SPEED = 0.5
  OBSTICAL_MIN_SPEED = 0.05
- OBSTICAL_SIZE = 25
+ OBSTICAL_SIZE = 10
  PLAYER_SIZE = 15
- PLAYER_SPEED = .3
+ PLAYER_SPEED = .5
  SCREEN = pygame.display.set_mode([500, 500])
  player_pos = [200,450]
  obsticals = []
  running = True
  restart = False
- while running and not restart:
-    #Obsticals update
-    #destroy old obsticals & update positions
-    for obstical in obsticals:
-        obstical[1] += obstical[2] 
-        if obstical[1] > OBSTICAL_MAX_Y_POS:
-            obsticals.remove(obstical)
-    #spawn new obsticals
-    while len(obsticals) < MAX_OBSTICALS:
-        obsticals.append([random.random() * OBSITCALS_X_POS_RANGE,-30,random.random() * OBSTICAL_MAX_SPEED + OBSTICAL_MIN_SPEED])
-    
-    #Input update
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_pos[0] > PLAYER_POS_RANGE[0]:
+ steps_count = 0
+ states = []
+ actions = []
+ rewards = tf.constant([], dtype=tf.float32)
+ dist = None
+ font = pygame.font.Font(None, 36) 
+ while running and not restart: 
+     
+  #take action
+  old_state = np.array([player_pos[0]])
+  if(len(obsticals) > 0):
+   for obstical in obsticals:
+        old_state = np.append(old_state,[obstical[0],obstical[1]])
+   dist = tfp.distributions.Categorical(probs=pg_model.model(old_state.reshape(1, -1),training=True),dtype=tf.float32)
+   action = dist.sample()[0].numpy()
+   if action == 1  and player_pos[0] > PLAYER_POS_RANGE[0]:
         player_pos[0] -= PLAYER_SPEED
-    if keys[pygame.K_RIGHT] and player_pos[0] < PLAYER_POS_RANGE[1]:
+   elif action == 2 and player_pos[0] < PLAYER_POS_RANGE[1]:
         player_pos[0] += PLAYER_SPEED
-    #Collision detection 
-    for obstical in obsticals:
-        if math.sqrt((player_pos[0] - obstical[0]) ** 2 + (player_pos[1] - obstical[1]) ** 2 ) < PLAYER_SIZE + OBSTICAL_SIZE:
-            restart = True
-            break
-    #Rendering
-    SCREEN.fill((255, 255, 255))
-    #render player
-    pygame.draw.circle(SCREEN, (0, 0, 255), (player_pos[0], player_pos[1]), PLAYER_SIZE)
-    #render obsticals
-    for obstical in obsticals:
-        pygame.draw.circle(SCREEN,(255,0,0),(obstical[0],obstical[1]),OBSTICAL_SIZE)
-    
-    pygame.display.flip()
-    
+  #Obsticals update
+  #destroy old obsticals & update positions
+  for obstical in obsticals:
+      obstical[1] += obstical[2] 
+      if obstical[1] > OBSTICAL_MAX_Y_POS:
+          obsticals.remove(obstical)
+  #spawn new obsticals
+  while len(obsticals) < MAX_OBSTICALS:
+      obsticals.append([random.random() * OBSITCALS_X_POS_RANGE,-30,random.random() * OBSTICAL_MAX_SPEED + OBSTICAL_MIN_SPEED])
+  
+  #Input update
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          running = False
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_LEFT] and player_pos[0] > PLAYER_POS_RANGE[0]:
+      player_pos[0] -= PLAYER_SPEED
+  if keys[pygame.K_RIGHT] and player_pos[0] < PLAYER_POS_RANGE[1]:
+      player_pos[0] += PLAYER_SPEED
+  #Collision detection 
+  for obstical in obsticals:
+      if math.sqrt((player_pos[0] - obstical[0]) ** 2 + (player_pos[1] - obstical[1]) ** 2 ) < PLAYER_SIZE + OBSTICAL_SIZE:
+          restart = True
+          break
+  #Rendering
+  SCREEN.fill((255, 255, 255))
+  #render player
+  pygame.draw.circle(SCREEN, (0, 0, 255), (player_pos[0], player_pos[1]), PLAYER_SIZE)
+  #render obsticals
+  for obstical in obsticals:
+      pygame.draw.circle(SCREEN,(255,0,0),(obstical[0],obstical[1]),OBSTICAL_SIZE)
+  
+  pygame.display.flip()
+  new_state = np.array([player_pos[0]])
+  for obstical in obsticals:
+        new_state = np.append(new_state,[obstical[0],obstical[1]])
+  if(dist != None):
+   actions.append(action)
+   states.append(old_state)
+   reward = None
+   if(rewards.shape[0]>0):
+    reward =  tf.constant([pg_model.compute_reward(state=new_state,action=action,previous_reward=rewards.numpy()[steps_count-2]) ],dtype=tf.float32)
+   else:
+    reward =  tf.constant([pg_model.compute_reward(state=new_state,action=action,previous_reward=0) ],dtype=tf.float32)
+   text_surface = font.render(str(reward.numpy()[0]), True, (255, 255, 255)) 
+   SCREEN.blit(text_surface, (500 // 2 - text_surface.get_width() // 2, 500 // 2 - text_surface.get_height() // 2))
+   rewards = tf.concat([rewards, reward], axis=0)
+  steps_count += 1
  if not running:
   pygame.quit()
  elif restart :
+     print(len(actions))
+     print(steps_count)
+     pg_model.update(states=states,actions=actions,rewards=rewards)
      start_game()
 
 
--- model.py
+++ model.py
@@ -1,31 +1,63 @@
 import tensorflow as tf
-
-class model:
+import os
+import tensorflow_probability as tfp
+import numpy as np
+class pg_model:
     #max_obsticals_count refers to the max number of obsticals the model can observe and it defines the number of inputs in the layer
-    def __init__(self,lr=1e-4,batch_norm=False,max_obsticals_count=5,activation='relu',load_from_disk = False,model_path='model.keras',output_activation='softmax') -> None:
+    def __init__(self,lr=1e-1,batch_norm=False,max_obsticals_count=20,activation='relu',load_from_disk = False,model_path='model.keras',output_activation='softmax',learning_rate=1e-4) -> None:
         self.lr = lr
+        self.max_obsticals_count = max_obsticals_count
         if not load_from_disk:
          self.__init_model__(batch_norm=batch_norm
                                           ,max_obsticals_count=max_obsticals_count,activation=activation,output_activation=output_activation)
         else:
             self.model = self.load(model_path=model_path)
+        self.init_optimizer(learning_rate=learning_rate)
     #max_obsticals_count refers to the max number of obsticals the model can observe and it defines the number of inputs in the layer    
     def __init_model__(self,batch_norm=False,max_obsticals_count=5,activation='relu',output_activation='softmax'):
         input = tf.keras.Input(max_obsticals_count * 2 + 1)
-        layer1 = tf.keras.layers.Dense(64,activation=activation)(input)
+        x = tf.keras.layers.Dense(64,activation=activation)(input)
         if batch_norm:
-         layer1 = tf.keras.layers.BatchNormalization()(layer1)
-        layer2 = tf.keras.layers.Dense(32,activation=activation)
+         x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dense(32,activation=activation)(x)
         if batch_norm:
-         layer2 = tf.keras.layers.BatchNormalization()(layer2)
-        output = tf.keras.layers.Dense(3,activation=output_activation)(layer2)
-        self.model = output 
+         x = tf.keras.layers.BatchNormalization()(x)
+        output = tf.keras.layers.Dense(3,activation=output_activation)(x)
+        self.model = tf.keras.models.Model(inputs=input,outputs=output) 
         
-    def update(self):
-        pass
-    def compute_reward(self):
-        pass
+    def init_optimizer(self,learning_rate=1e-4):    
+        self.optimizer =  tf.keras.optimizers.SGD(learning_rate)
+    def update(self,states,actions,rewards,auto_save=False,output_path='model.keras'):
+        
+        for state,reward, action in zip(states,rewards, actions):
+         with tf.GradientTape() as tape:
+           p = self.model(state.reshape(1,-1),training=True)
+           dist = tfp.distributions.Categorical(p)
+           log_prob = dist.log_prob(action)
+           loss = -log_prob*reward
+           gradients = tape.gradient(loss, self.model.trainable_variables)
+         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        if auto_save:
+            self.save(output_path=output_path)
+             
+    def compute_reward(self,state,action,previous_reward,discount_factor=0.6,hit=False):
+        reward = previous_reward * discount_factor
+        if hit:
+            reward = -10
+            return reward
+        if action!= 0:
+           reward -= 0.4
+        for i in range(self.max_obsticals_count):
+            player_x_pos = state[0]
+            obstical_x_pos = state[i*2 +1]
+            obstical_y_pos = state[i*2 +2]
+            a = obstical_y_pos ** 2 * 10
+            b = abs(obstical_x_pos - player_x_pos) - 1
+            reward += a * b
+        return reward
     def save(self,output_path='model.keras'):
-        pass
+         if not os.path.exists(output_path):
+            os.mkdir(output_path)
+         self.model.save(output_path)
     def load(self,model_path='model.keras'):
-        pass
+        self.model = tf.keras.models.load_model(model_path)
